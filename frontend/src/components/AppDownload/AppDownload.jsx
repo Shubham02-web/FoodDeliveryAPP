8b882bd9bb9fec26438d2036fbf735bc4288b51f
Shubham Vishwakarma <vi453770@gmail.com>
2025-01-23 17:21:08 +0530

    our pp download component is ready and almonst our home page is ready

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,18 @@
+import "./AppDownload.css";
+import React from "react";
+import { assets } from "../../assets/assets";
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br /> Tomato App
+      </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="playStore Icon" />
+        <img src={assets.app_store} alt="App Store Icon" />
+      </div>
+    </div>
+  );
+};
+
+export default AppDownload;
