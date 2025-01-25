a136b1733e59a508d9764784ac157f2919bdc8c7
Shubham Vishwakarma <vi453770@gmail.com>
2025-01-26 02:50:32 +0530

    remove from cart functionality added

--- backend/controllers/CartController.js
+++ backend/controllers/CartController.js
@@ -4,7 +4,7 @@ import UserModel from "../models/UserModels.js";
 // add to user cart
 const addToCart = async(req, res) => {
     try {
-        let userData = await userModel.findOne({ _id: req.body.userId });
+        let userData = await userModel.findById(req.body.userId);
         let cartData = await userData.cartData;
         if (!cartData[req.body.itemId]) {
             cartData[req.body.itemId] = 1;
@@ -24,7 +24,23 @@ const addToCart = async(req, res) => {
 
 // remove item from user cart
 
-const removeFromCart = async(req, res) => {};
+const removeFromCart = async(req, res) => {
+    try {
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if (cartData[req.body.itemId] > 0) {
+            cartData[req.body.itemId] -= 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+        res.status(200).json({ success: true, message: "Removed From Cart" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Error in Remove From Cart API",
+        });
+    }
+};
 
 // Fetch User Cart Data
 
