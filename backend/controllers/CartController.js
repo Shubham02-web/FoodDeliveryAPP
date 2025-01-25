c6cc707f7458eeed967294584562d8991f29f232
Shubham Vishwakarma <vi453770@gmail.com>
2025-01-26 02:40:10 +0530

    add to cart API created with user Authentication using JWT

--- backend/controllers/CartController.js
+++ backend/controllers/CartController.js
@@ -1,7 +1,26 @@
+import userModel from "../models/UserModels.js";
 import UserModel from "../models/UserModels.js";
 
 // add to user cart
-const addToCart = async(req, res) => {};
+const addToCart = async(req, res) => {
+    try {
+        let userData = await userModel.findOne({ _id: req.body.userId });
+        let cartData = await userData.cartData;
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1;
+        } else {
+            cartData[req.body.itemId] += 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+        res.status(201).json({ success: true, message: "Added To Cart" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Error in Add To Cart API",
+        });
+    }
+};
 
 // remove item from user cart
 
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,22 @@
 import jwt from "jsonwebtoken";
 
-const authMiddleware = async(req, res, next) => {};
+const authMiddleware = async(req, res, next) => {
+    const { token } = req.headers;
+    if (!token) {
+        return res
+            .status(401)
+            .json({ success: false, message: "Not Authorized Login Again" });
+    }
+    try {
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        req.body.userId = token_decode.id;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success: false,
+            message: "error in user auth token",
+        });
+    }
+};
 export default authMiddleware;
